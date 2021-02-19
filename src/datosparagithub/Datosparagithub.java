6188ef142307c98cf219a664b52eeb34b0496642
salva <salva@LAPTOP-B2KFIIUB>
2021-02-18 23:34:50 -0600

    estos son mis datos

---
+++ src/datosparagithub/Datosparagithub.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datosparagithub;
+
+/**
+ *
+ * @author salva
+ */
+public class Datosparagithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String nombre = "Salvador Moises Alberto Valle Saenz";
+        System.out.println("Mi nombre es = " + nombre );
+    String matricula =  "66136" ;
+        System.out.println("con matricula = " +  matricula);
+        
+    }
+    
+}
