0349d931c4e96c219a3a45203db4add96afc2540
salva <salva@LAPTOP-B2KFIIUB>
2021-02-23 20:55:52 -0600

    DATOS PRUEBA GITHUB

---
+++ src/GitGrupoA66136/GitGrupoA66136.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GitGrupoA66136;
+
+/**
+ *
+ * @author salva
+ */
+public class GitGrupoA66136 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String nombre = "Salvador Moises Alberto Valle Saenz";
+        System.out.println("Mi nombre es = " + nombre );
+    String matricula =  "66136" ;
+        System.out.println("con matricula = " +  matricula);
+        
+    }
+    
+}
